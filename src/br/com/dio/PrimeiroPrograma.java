e8db5af39af4bb8fc2a90e8b2c8a42bc6c3038d2
gabriel-silva <gabrielnasilva88@gmail.com>
2023-03-20 21:58:27 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+/*        int a = 8;
+        int b = 4;
+        System.out.println("Hello Word!" + (a+b));*/
+    }
+}
