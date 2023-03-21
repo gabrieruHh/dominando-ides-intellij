471c7a41787dc5d17d7b61357e5fcdb557c29b7c
gabriel-silva <gabrielnasilva88@gmail.com>
2023-03-20 22:17:52 -0300

    creating book class

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,12 +5,47 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livro livro = new Livro("Abra Cadabra", 300);
 
-
+        System.out.println(livro);
         System.out.println(gato);
 
 /*        int a = 8;
         int b = 4;
         System.out.println("Hello Word!" + (a+b));*/
+    }
+
+    static class Livro{
+        private String nome;
+        private Integer numpag;
+
+        public Livro(String nome, Integer numpag) {
+            this.nome = nome;
+            this.numpag = numpag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumpag() {
+            return numpag;
+        }
+
+        public void setNumpag(Integer numpag) {
+            this.numpag = numpag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numpag=" + numpag +
+                    '}';
+        }
     }
 }
